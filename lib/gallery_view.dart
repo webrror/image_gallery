496ae6649cae13d79c308d8c2bec40af189d3480
Jerin <jerinjacob0206@gmail.com>
2022-11-25 00:59:22 +0530

    undo test 5

--- lib/gallery_view.dart
+++ lib/gallery_view.dart
@@ -10,12 +10,10 @@ import 'package:gallery_view/view_photo.dart';
 class GalleryView extends StatelessWidget {
   final List<String> imageUrlList;
   final int crossAxisCount;
-  final BuildContext context;
   const GalleryView({
     Key? key,
     required this.imageUrlList,
     required this.crossAxisCount,
-    required this.context,
   }) : super(key: key);
 
   static const MethodChannel _channel = const MethodChannel('gallery_view');
